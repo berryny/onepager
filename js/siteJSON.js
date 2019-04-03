c16accef79d96e2f823634a0ef9732db14f2d307
J D <judesire@yahoo.com>
2019-04-03 17:09:16 -0400

    Add files via upload

--- js/siteJSON.js
+++ js/siteJSON.js
@@ -82,9 +82,6 @@ $.when(
 		console.log('error');		
 	}
 
-	
-});
-
 	var map;
 	      function initMap() {
 	        map = new google.maps.Map(document.getElementById('map'), {
@@ -94,16 +91,16 @@ $.when(
 	        });
 
 	        // Create a <script> tag and set the USGS URL as the source.
-	        var script = document.createElement('script');
+	        //var script = document.createElement('script');
 	        // This example uses a local copy of the GeoJSON stored at
 	        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-	        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-	        document.getElementsByTagName('head')[0].appendChild(script);
+	        //script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+	        //document.getElementsByTagName('head')[0].appendChild(script);
 	      }
 
 	      // Loop through the results array and place a marker for each
 	      // set of coordinates.
-	      window.eqfeed_callback = function(results) {
+/*	      window.eqfeed_callback = function(results) {
 	        for (var i = 0; i < results.features.length; i++) {
 	          var coords = results.features[i].geometry.coordinates;
 	          var latLng = new google.maps.LatLng(coords[1],coords[0]);
@@ -113,7 +110,7 @@ $.when(
 	          });
 	        }
 	      }
-/*
+*/
 	      window.eqfeed_callback = function(results) {
 	        for (var i = 0; i < nycMarkets.length; i++) {
 	          var coords = nycMarkets[i].coordinates;
@@ -124,4 +121,5 @@ $.when(
 	          });
 	        }
 	      }
-*/
+	
+});
