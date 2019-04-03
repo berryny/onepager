5d3a7a1ff807876604b7fbcd76b0b1d2ec46fc90
J D <judesire@yahoo.com>
2019-04-03 16:38:52 -0400

    add google map

--- js/siteJSON.js
+++ js/siteJSON.js
@@ -82,4 +82,32 @@ $.when(
 		console.log('error');		
 	}
 
+	var map;
+	      function initMap() {
+	        map = new google.maps.Map(document.getElementById('map'), {
+	          zoom: 2,
+	          center: new google.maps.LatLng(2.8,-187.3),
+	          mapTypeId: 'terrain'
+	        });
+
+	        // Create a <script> tag and set the USGS URL as the source.
+	       // var script = document.createElement('script');
+	        // This example uses a local copy of the GeoJSON stored at
+	        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+	        //script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+	       // document.getElementsByTagName('head')[0].appendChild(script);
+	      }
+
+	      // Loop through the results array and place a marker for each
+	      // set of coordinates.
+	      window.eqfeed_callback = function(results) {
+	        for (var i = 0; i < nycMarkets.length; i++) {
+	          var coords = nycMarkets[i].coordinates;
+	          var latLng = new google.maps.LatLng(coords[1],coords[0]);
+	          var marker = new google.maps.Marker({
+	            position: latLng,
+	            map: map
+	          });
+	        }
+	      }
 });
