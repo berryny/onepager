9483fd76182e1a844e43e10d2517e0b5556bdeb4
J D <judesire@yahoo.com>
2019-04-04 05:46:04 -0400

    Add files via upload

--- js/siteJSON.js
+++ js/siteJSON.js
@@ -83,45 +83,27 @@ $.when(
 	}
 
 
-	var map;
-	      function initMap() {
-	        map = new google.maps.Map(document.getElementById('map'), {
-	          zoom: 2,
-	          center: new google.maps.LatLng(2.8,-187.3),
-	          mapTypeId: 'terrain'
-	        });
 
-	        // Create a <script> tag and set the USGS URL as the source.
-	        //var script = document.createElement('script');
-	        // This example uses a local copy of the GeoJSON stored at
-	        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-	        //script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-	        //document.getElementsByTagName('head')[0].appendChild(script);
-	      }
-
-	      // Loop through the results array and place a marker for each
-	      // set of coordinates.
-/*	      window.eqfeed_callback = function(results) {
-	      	console.log('results',results)
-	        for (var i = 0; i < results.features.length; i++) {
-	          var coords = results.features[i].geometry.coordinates;
-	          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-	          var marker = new google.maps.Marker({
-	            position: latLng,
-	            map: map
-	          });
-	        }
-	      }
-*/
-	     // window.eqfeed_callback = function(results) {
-	        for (var i = 0; i < nycMarkets.length; i++) {
-	          var coords = nycMarkets[i].location_point.coordinates;
-	          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-	          var marker = new google.maps.Marker({
-	            position: latLng,
-	            map: map
-	          });
-	        }
-	   //   }
 	
 });
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 2,
+			center: new google.maps.LatLng(2.8,-187.3),
+			mapTypeId: 'terrain'
+		});
+	}
+
+	$.getJSON( "https://data.cityofnewyork.us/resource/94pk-v63f.json", function( json ) {
+		var nycMarkets =  json[0];
+
+		for (var i = 0; i < nycMarkets.length; i++) {
+			var coords = nycMarkets[i].location_point.coordinates;
+			var latLng = new google.maps.LatLng(coords[1],coords[0]);
+			var marker = new google.maps.Marker({
+				position: latLng,
+				map: map
+			});
+		}
+	});
